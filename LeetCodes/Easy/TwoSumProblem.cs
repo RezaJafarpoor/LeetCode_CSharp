5b823a4bd7e94bdc63d8bb763c3cd6418b7c52bc
reza <rezajafarpoor222@gmail.com>
2024-09-16 22:08:21 +0330

    TwoSum problem

---
+++ LeetCodes/Easy/TwoSumProblem.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable All
+namespace LeetCodes.Easy;
+
+public class TwoSumProblem
+{
+
+    public int[] TwoSum(int[] input, int target)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (i == j)
+                {
+                     continue;
+                }
+                if (input[i] + input[j] == target)
+                {
+                    return new [] { i, j };
+                }
+            }
+        }
+
+        return [];
+    }
+}
--- LeetCodes/Program.cs
+++ LeetCodes/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using LeetCodes.Easy;
+
+var array = new int[] { 1, 2, 3, 4, 5 };
+var target = 7;
+var t = new TwoSumProblem();
+var result = t.TwoSum(array, target);
+foreach (var VARIABLE in result)
+{
+    Console.WriteLine(VARIABLE);
+}
